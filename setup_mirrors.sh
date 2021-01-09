39731480a094aee087d5feda1190e6a464512eab
pedromorgan <pedromorgan@gmail.com>
2021-01-09 03:59:50 +0000

    mirror flag

--- setup_mirrors.sh
+++ setup_mirrors.sh
@@ -2,29 +2,35 @@
 
 cd /home/fg
 
-
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/fgms/fgtracker fgtracker
 
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/fgms/src fgms
 
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/flightgear 
 
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/simgear
 
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/fgmeta
 
-
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/fgrun
 
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/openradar
 
-
-
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/terragear
 
-
+echo "-----------------------------------"
 git clone --mirror https://git.code.sf.net/p/flightgear/sceneryweb
 
+echo "-----------------------------------"
+echo "Done"
 
 
 
