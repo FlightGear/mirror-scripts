75be016af94e22cb3ca5d2f75eb89fb4ebb7eba4
pedromorgan <pedromorgan@gmail.com>
2021-01-09 03:56:02 +0000

    mirror flag

--- setup_mirrors.sh
+++ setup_mirrors.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-git clone https://git.code.sf.net/p/fgms/fgtracker fgtracker
-
-git clone https://git.code.sf.net/p/fgms/src fgms
-
-git clone https://git.code.sf.net/p/flightgear/flightgear 
-
-git clone https://git.code.sf.net/p/flightgear/simgear
-
-git clone https://git.code.sf.net/p/flightgear/fgmeta
+cd /home/fg
 
 
-git clone https://git.code.sf.net/p/flightgear/fgrun
+git clone --mirror https://git.code.sf.net/p/fgms/fgtracker fgtracker
 
-git clone https://git.code.sf.net/p/flightgear/openradar
+git clone --mirror https://git.code.sf.net/p/fgms/src fgms
+
+git clone --mirror https://git.code.sf.net/p/flightgear/flightgear 
+
+git clone --mirror https://git.code.sf.net/p/flightgear/simgear
+
+git clone --mirror https://git.code.sf.net/p/flightgear/fgmeta
+
+
+git clone --mirror https://git.code.sf.net/p/flightgear/fgrun
+
+git clone --mirror https://git.code.sf.net/p/flightgear/openradar
 
 
 
-git clone https://git.code.sf.net/p/flightgear/terragear
+git clone --mirror https://git.code.sf.net/p/flightgear/terragear
 
 
-git clone https://git.code.sf.net/p/flightgear/sceneryweb
+git clone --mirror https://git.code.sf.net/p/flightgear/sceneryweb
 
 
 
